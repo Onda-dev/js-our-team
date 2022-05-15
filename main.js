a292303a4199347d8ad05826d947b98366490179
Onda-dev <manuelaltieri3@gmail.com>
2022-05-15 12:11:08 +0200

    Prova Bonus

--- main.js
+++ main.js
@@ -53,3 +53,29 @@ for (let i = 0; i < teamMembers.length; i++) {
 // 3b. Incollo le card nell'HTML
 teamMembersContainer.innerHTML += teamCard
 }
+
+// BONUS:
+// Utilizzare gli input presenti nella pagina per permettere all'utente di aggiungere nuovi membri del team: cliccando sul pulsante "add" viene creato un nuovo oggetto, il quale viene inserito nell'array iniziale e viene stampata una nuova card con tutte le informazioni inserite dall'utente.
+
+// 1. Creo la funzione che si attiva cliccando sul tasto "Add"
+document.getElementById("addMemberButton").addEventListener("click", createNewMember)
+
+function createNewMember(){
+    // 2. Estrapolo dall'HTML i valori necessari alla creazione di un nuovo membro
+    const newMemberName = document.getElementById("name").value
+    console.log(newMemberName)
+    const newMemberRole = document.getElementById("role").value
+    console.log(newMemberRole)
+    const newMemberProPic = document.getElementById("image").value
+    console.log(newMemberProPic)
+    // 3. Creo un oggetto "nuovo utente"
+    newTeamMember = {}
+    // 3b. Ci inserisco le info prese dal form
+    newTeamMember.name = newMemberName
+    newTeamMember.role = newMemberRole
+    newTeamMember.proPic = newMemberProPic
+    // // 4. Pusho nell'array che contiene i membri del team il nuovo membro
+    teamMembers.push(newTeamMember)
+}
+
+
